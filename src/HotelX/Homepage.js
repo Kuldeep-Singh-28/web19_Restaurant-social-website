d67718c83bf9e9d2b5b6cfcdf67c9997b017f07a
ujjawal mittal <ujjawalmittal55@gmail.com>
2021-06-24 22:43:23 +0530

    Final changes for today

--- src/HotelX/Homepage.js
+++ src/HotelX/Homepage.js
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactPlayer from 'react-player'
 import {Container,Row,Col} from 'react-bootstrap'
 import FormatQuoteRoundedIcon from '@material-ui/icons/FormatQuoteRounded';
+import StarHalfIcon from '@material-ui/icons/StarHalf';
+
 import './styles/hotel.css'
+import StarIcon from '@material-ui/icons/Star';
 function Homepage() {
     return (
         <div>
@@ -12,10 +15,19 @@ function Homepage() {
                 <ReactPlayer  className='react-player' url='https://www.youtube.com/watch?v=ysz5S6PUM-U'  width='450px' height='400px' />
                 </Col>
                 <Col>
-                    <div className='comment'>
+                    <div className='comment com-con'>
                         <FormatQuoteRoundedIcon className="quote"/>
-                        this is awesome i like very much!
+                      <div className='py-.5 mx-5'>
+                      this is awesome i like very much! 
+                      <StarIcon/> 
+                      <StarIcon/> 
+                      <StarIcon/> 
+                      <StarIcon/> 
+                      <StarHalfIcon/>
+                      </div> 
+                      <FormatQuoteRoundedIcon className="quote2"/>
                     </div>
+                    
                 </Col>
             </Row>
           
