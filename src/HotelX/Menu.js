86a1f5c59f2f5b2615514e72a3f79beaac998e4d
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-06-24 20:22:23 +0530

    Made each carousel a a link to its respective page

--- src/HotelX/Menu.js
+++ src/HotelX/Menu.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import Carousel from "./Carousel";
 import Style from "./styles/Menu.module.css";
 
@@ -7,13 +8,19 @@ const Menu = () => {
     return (
         <Container fluid>
             <div className={Style.Starters}>
-                <Carousel />
+                <Link to="/starters">
+                    <Carousel />
+                </Link>
             </div>
-            <div className={Style.Starters}>
-                <Carousel />
+            <div className={Style.Main_course}>
+                <Link to="/main_course">
+                    <Carousel />
+                </Link>
             </div>
-            <div className={Style.Starters}>
-                <Carousel />
+            <div className={Style.Dessert}>
+                <Link to="/dessert">
+                    <Carousel />
+                </Link>
             </div>
         </Container>
     );
