2c0e34dfe2c5241f81e3bc1eccc5d781955076f8
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-06-24 20:02:02 +0530

    Added in the required carousels

---
+++ src/HotelX/Carousel.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import { Carousel } from "react-bootstrap";
+
+function ControlledCarousel() {
+    const [index, setIndex] = useState(0);
+
+    const handleSelect = (selectedIndex, e) => {
+        setIndex(selectedIndex);
+    };
+
+    const w = {
+        height: "40vh",
+        objectFit: "cover",
+    };
+
+    return (
+        <Carousel activeIndex={index} onSelect={handleSelect}>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    style={w}
+                    src="./test1.jpg"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h3>First slide label</h3>
+                    <p>
+                        Nulla vitae elit libero, a pharetra augue mollis
+                        interdum.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="./test1.jpg"
+                    style={w}
+                    alt="Second slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Second slide label</h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing
+                        elite.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <img
+                    className="d-block w-100"
+                    src="./x.jpg"
+                    style={w}
+                    alt="Third slide"
+                />
+
+                <Carousel.Caption>
+                    <h3>Third slide label</h3>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl
+                        consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+}
+
+export default ControlledCarousel;
