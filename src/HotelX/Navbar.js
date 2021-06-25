345f4775809eff8f4a4074869b01570e08b37a34
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-06-25 20:19:21 +0530

    Today's changes

--- src/HotelX/Navbar.js
+++ src/HotelX/Navbar.js
@@ -5,11 +5,20 @@ import "./styles/Navbar.css";
 function NavBar() {
     return (
         <div className="Nav" className="n1 mb-5">
-                <Nav className="mr-auto" className="n">
-                    <Nav.Link href="/" className="n2">Home</Nav.Link>
-                    <Nav.Link href="#features" className="n2">Features</Nav.Link>
-                    <Nav.Link href="/menu" className="n2">Menu</Nav.Link>
-                </Nav>
+            <Nav className="mr-auto" className="n">
+                <Nav.Link href="/" className="n2">
+                    Home
+                </Nav.Link>
+                <Nav.Link href="#features" className="n2">
+                    Features
+                </Nav.Link>
+                <Nav.Link href="/menu" className="n2">
+                    Menu
+                </Nav.Link>
+                <Nav.Link href="/Login" className="n2">
+                    Login
+                </Nav.Link>
+            </Nav>
         </div>
     );
 }
