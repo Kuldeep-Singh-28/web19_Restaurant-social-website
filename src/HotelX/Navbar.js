b033dcbd624cba98ae993b5e2a25791c67ffefa4
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-06-25 21:52:03 +0530

    Removed the margin bottom from the navbar.

--- src/HotelX/Navbar.js
+++ src/HotelX/Navbar.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link ,Redirect} from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 import "./styles/Navbar.css";
 
@@ -11,34 +11,34 @@ function SignOut() {
     const [user] = useAuthState(auth);
     return (
         auth.currentUser && (
-            
-           <div>
-               <span className="sign-out" onClick={() => auth.signOut()}>
-                Sign Out
-                
-            </span>
-            <span className="mx-3">
-                welcome {user.displayName}
-            </span>
-           </div>
-            
+            <div>
+                <span className="sign-out" onClick={() => auth.signOut()}>
+                    Sign Out
+                </span>
+                <span className="mx-3">welcome {user.displayName}</span>
+            </div>
         )
-    )
-        }
+    );
+}
 function NavBar() {
     const [user] = useAuthState(auth);
     return (
-        <div className="Nav" className="n1 mb-5">
-
-            <img src="/logo.jpg" className="_logo"/>
-                <Nav className="mr-auto" className="n">
-                    <Nav.Link href="/" className="n2">Home</Nav.Link>
-                    <Nav.Link href="#features" className="n2">Features</Nav.Link>
-                    <Nav.Link href="/menu" className="n2">Menu</Nav.Link>
-                    <Nav.Link href="/login" className="n2">{user ? <SignOut/>:<span>login</span>}</Nav.Link>
-                </Nav>
-
-          
+        <div className="Nav" className="n1">
+            <img src="/logo.jpg" className="_logo" />
+            <Nav className="mr-auto" className="n">
+                <Nav.Link href="/" className="n2">
+                    Home
+                </Nav.Link>
+                <Nav.Link href="#features" className="n2">
+                    Features
+                </Nav.Link>
+                <Nav.Link href="/menu" className="n2">
+                    Menu
+                </Nav.Link>
+                <Nav.Link href="/login" className="n2">
+                    {user ? <SignOut /> : <span>login</span>}
+                </Nav.Link>
+            </Nav>
         </div>
     );
 }
