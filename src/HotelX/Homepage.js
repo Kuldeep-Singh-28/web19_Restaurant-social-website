246dbce398905c2793f48444d9efbc07b1a61c61
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-06-26 19:47:03 +0530

    Removed the home_carousel due to resizing issues

--- src/HotelX/Homepage.js
+++ src/HotelX/Homepage.js
@@ -6,7 +6,6 @@ import StarHalfIcon from "@material-ui/icons/StarHalf";
 
 import "./styles/hotel.css";
 import StarIcon from "@material-ui/icons/Star";
-import Home_carousel from "./Home_carousel";
 
 function ControlledCarousel() {
     const [index, setIndex] = useState(0);
@@ -101,7 +100,6 @@ function Homepage() {
     //IITISoC---Restaurant-social-website
     return (
         <div>
-            <Home_carousel />
             <Container className="navbarx" fluid={true}>
                 <Row className="mb-4" style={{ minHeight: "41vh" }}>
                     <Col sm={5} className="mb-3" style={{ minHeight: "41vh" }}>
