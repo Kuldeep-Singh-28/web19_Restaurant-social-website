3f78006fa7bed90a049a0f121405840359649d64
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-06-28 14:08:10 +0530

    minor changes to the homepageCarousel

--- src/HotelX/HomePageCorusel.js
+++ src/HotelX/HomePageCorusel.js
@@ -1,33 +1,32 @@
-import React from 'react'
-import './styles/HomePageCorusel.css'
+import React from "react";
+import "./styles/HomePageCorusel.css";
 function HomePageCorusel() {
     return (
         <div>
-            <div class="slider">
-	<div class="slide-track">
-		<div class="slide">
-			<img src="/food2.jpg" height="100" width="250" alt="" />
-		</div>
-		<div class="slide">
-			<img src="/food2.jpg" height="100" width="250" alt="" />
-		</div>
-		<div class="slide">
-			<img src="/food2.jpg" height="100" width="250" alt="" />
-		</div>
-		<div class="slide">
-			<img src="/food2.jpg" height="100" width="250" alt="" />
-		</div>
-		<div class="slide">
-			<img src="/food2.jpg" height="100" width="250" alt="" />
-		</div>
-		<div class="slide">
-			<img src="/food2.jpg" height="100" width="250" alt="" />
-		</div>
-	
-	</div>
-</div>
+            <div className="slider">
+                <div className="slide-track">
+                    <div className="slide">
+                        <img src="/food2.jpg" height="400" width="250" alt="" />
+                    </div>
+                    <div className="slide">
+                        <img src="/food2.jpg" height="100" width="250" alt="" />
+                    </div>
+                    <div className="slide">
+                        <img src="/food2.jpg" height="100" width="250" alt="" />
+                    </div>
+                    <div className="slide">
+                        <img src="/food2.jpg" height="100" width="250" alt="" />
+                    </div>
+                    <div className="slide">
+                        <img src="/food2.jpg" height="100" width="250" alt="" />
+                    </div>
+                    <div className="slide">
+                        <img src="/food2.jpg" height="100" width="250" alt="" />
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
 
-export default HomePageCorusel
+export default HomePageCorusel;
