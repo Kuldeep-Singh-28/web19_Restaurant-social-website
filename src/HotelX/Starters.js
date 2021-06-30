1998eab50a2d42c708a4e60b8736c255bcb9fcff
ujjawal mittal <ujjawalmittal55@gmail.com>
2021-06-30 20:04:15 +0530

    starterpage

--- src/HotelX/Starters.js
+++ src/HotelX/Starters.js
@@ -1,8 +1,11 @@
 import React,{ useState,useEffect} from 'react'
-import {storage} from './firebase'
+import db ,{storage} from './firebase'
 import "./styles/starters.css"
+import {Card,Typography} from "@material-ui/core"
+import AddShoppingCartIcon from '@material-ui/icons/AddShoppingCart';
 function Starters() {
     const[files,setFiles]= useState([]);
+    const starter = db.collection("dishes").doc("dish").collection("starters");
     useEffect(() => {
         var storageRef = storage.ref("images");
         const fetchImages = async () => {
@@ -16,17 +19,29 @@ function Starters() {
         
         const loadImages = async () => {
             const urls = await fetchImages();
-            setFiles(urls);
+            
+         
+            setFiles(urls)
         }
         loadImages();
         }, []);
         
+        const addToCart=e=>{
+                console.log("hello")
+        }
     return (
         <div className="_starters">
-            {files.map((x)=>{
-                <img src={x} style={{filter:'none' ,height:"100px"}}/>
-            })}
-            <img src={files[0]} style={{filter:'none'}}/>
+            {files.map((item,index) => (
+   <Card key={index} className="_box col-sm-12 ">
+      <img src={ item } className=" _imgS" style={{filter: 'none',height:"30vh",width:"30vh"}} />
+      <Typography variant="h5" component="h2" className="_typo">
+            How are you ?
+            
+          </Typography>
+          <AddShoppingCartIcon style={{ color: "#7b877c", size: 0.5 }} className="_cart" onClick={addToCart}/>
+   </Card>
+))}
+            
         </div>
     )
 }
