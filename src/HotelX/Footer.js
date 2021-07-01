c78a1d65ad8bfc471b11663613fcf6f33e220874
codemaster2911 <2911.tushargoyal@gmail.com>
2021-07-01 13:48:12 +0530

    added login.html

--- src/HotelX/Footer.js
+++ src/HotelX/Footer.js
@@ -2,51 +2,59 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Container, Row, Col, Carousel } from "react-bootstrap";
 import Foo_style from "./styles/Footer.module.css";
+import InstagramIcon from "@material-ui/icons/Instagram";
+import FacebookIcon from "@material-ui/icons/Facebook";
 function Footer() {
-    return (
-        <div className={Foo_style.footer}>
-            <Container fluid>
-                <Row>
-                    <Col sm={3}>
-                        <p>LOGO</p>
-                    </Col>
-                    <Col sm={3} className="px-1">
-                        <ul>
-                            <li className={Foo_style.hotel_name}>
-                                <a href="/">UJJU HOTEL</a>
-                            </li>
-                            <li className="">
-                                <a href="/menu">MENU</a>
-                            </li>
-                            <li className="text-muted">ujju@gmail.com</li>
-                            <li className="">1234567890</li>
-                            <li className="text-muted">
-                                <a href="/">PRIVACY POLICY</a>
-                            </li>
-                        </ul>
-                    </Col>
-                    <Col sm={3} className="px-1">
-                        <p>
-                            Lorem ipsum dolor sit amet consectetur adipisicing
-                            elit. Obcaecati molestiae quas commodi incidunt a,
-                            recusandae aliquid pariatur dolorum aut optio porro
-                            dignissimos dolores eaque necessitatibus esse
-                            perferendis doloremque! Vitae, rerum?
-                        </p>
-                    </Col>
-                    <Col sm={3} className="px-1">
-                        <p>
-                            Lorem, ipsum dolor sit amet consectetur adipisicing
-                            elit. Soluta amet tempore mollitia provident
-                            molestiae voluptas recusandae autem odio deleniti,
-                            iusto dolor consequuntur voluptatum minus sint.
-                            Possimus voluptatibus corporis deserunt eligendi!
-                        </p>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
-    );
+  return (
+    <div className={Foo_style.footer}>
+      <Container fluid>
+        <Row>
+          <Col sm={3}>
+            <p>LOGO</p>
+          </Col>
+          <Col sm={3} className="px-1">
+            <ul>
+              <li className={Foo_style.hd}>
+                <a href="/">HotelX</a>
+              </li>
+              <li className={Foo_style.bd}>
+                <a href="#">Home</a>
+              </li>
+              <li className={Foo_style.bd}>
+                <a href="#">Menu</a>
+              </li>
+              <li className={Foo_style.bd}>
+                <a href="#">My Cart</a>
+              </li>
+            </ul>
+          </Col>
+          <Col sm={3} className="px-1">
+            <ul>
+              <li className={Foo_style.hd}>Our Company</li>
+              <li className={Foo_style.bd}>
+                <a href="#">Locate us</a>
+              </li>
+              <li className={Foo_style.bd}>Contact Us</li>
+              <li className={Foo_style.bd}>Privacy Policy</li>
+            </ul>
+          </Col>
+          <Col sm={3} className="px-1">
+            <ul>
+              <li className={Foo_style.hd}>Follow us</li>
+              <li className={Foo_style.bd}>
+                <FacebookIcon color="secondary" className="mx-2" />
+                <a href="#">Facebook</a>
+              </li>
+              <li className={Foo_style.bd}>
+                <InstagramIcon color="secondary" className="mx-2" />
+                <a href="#">Instagram</a>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
 }
 
 export default Footer;
