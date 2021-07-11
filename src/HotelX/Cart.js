06fa6277bfe7eee2f850adeba730c3bcd9730db9
ujjawal mittal <ujjawalmittal55@gmail.com>
2021-07-11 18:12:27 +0530

    cart

--- src/HotelX/Cart.js
+++ src/HotelX/Cart.js
@@ -11,24 +11,29 @@ function Cart() {
     const[item,setItem]=useState([])
     
 
-
+    
     useEffect(() => {
         if(user){
+            console.log(user)
         const cart =  db.collection("users").doc(user.uid).collection("My-cart");
 
         cart.onSnapshot(
             snapshot => {
+                console.log(snapshot.docs.map(doc => doc.data().name))
                 setItem(snapshot.docs.map(doc => doc.data()))
             }
         )}
-    },[])
+    },[user])
+    console.log(item)
     return (
         <div className="_cart">
-            {item.map(i =>{
-                <Typography variant="h5" component="h2" className="_typo">
+            he
+            {item && item.map((i,index) =>{
+                <div key={index} variant="h5" component="h2" className="_typo">
+                    hello
                 {i.name}
                 {i.price}
-              </Typography>
+              </div>
             }) }
         </div>
     )
