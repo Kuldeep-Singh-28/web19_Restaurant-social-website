d00adafe607e133aac45344e4a4ec131a6026170
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-07-13 04:14:08 +0530

    changes made to the comment carousel

--- src/HotelX/Homepage.js
+++ src/HotelX/Homepage.js
@@ -9,6 +9,7 @@ import StarIcon from "@material-ui/icons/Star";
 import Middle1 from "./Middle1";
 import Middle2 from "./Middle2";
 import Middle3 from "./Middle3";
+import Comment from "./Comment";
 
 function ControlledCarousel() {
     const [index, setIndex] = useState(0);
@@ -23,79 +24,129 @@ function ControlledCarousel() {
     };
 
     return (
-        <Carousel activeIndex={index} onSelect={handleSelect}>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    style={w}
-                    src="./food2.jpg"
-                    alt="First slide"
-                />
-                <Carousel.Caption>
-                    <div className="comment com-con">
-                        <FormatQuoteRoundedIcon className="quote" />
-                        <div className="mx-5 comment-text">
-                            This is awesome i like very much!
-                            <br />
-                            <StarIcon />
-                            <StarIcon />
-                            <StarIcon />
-                            <StarIcon />
-                            <StarHalfIcon />
+        <div
+            id="carouselExampleCaptions"
+            class="carousel slide"
+            data-mdb-ride="carousel"
+        >
+            <div class="carousel-indicators">
+                <button
+                    type="button"
+                    data-mdb-target="#carouselExampleCaptions"
+                    data-mdb-slide-to="0"
+                    class="active"
+                    aria-current="true"
+                    aria-label="Slide 1"
+                ></button>
+                <button
+                    type="button"
+                    data-mdb-target="#carouselExampleCaptions"
+                    data-mdb-slide-to="1"
+                    aria-label="Slide 2"
+                ></button>
+                <button
+                    type="button"
+                    data-mdb-target="#carouselExampleCaptions"
+                    data-mdb-slide-to="2"
+                    aria-label="Slide 3"
+                ></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img
+                        className="d-block w-100"
+                        style={w}
+                        src="./food2.jpg"
+                        alt="First slide"
+                    />
+                    <div class="carousel-caption d-none d-md-block">
+                        <div className="comment com-con">
+                            <FormatQuoteRoundedIcon className="quote" />
+                            <div className="mx-5 comment-text">
+                                This is awesome i like very much!
+                                <br />
+                                <StarIcon />
+                                <StarIcon />
+                                <StarIcon />
+                                <StarIcon />
+                                <StarHalfIcon />
+                            </div>
+                            <FormatQuoteRoundedIcon className="quote2" />
                         </div>
-                        <FormatQuoteRoundedIcon className="quote2" />
                     </div>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="./food3.jpg"
-                    style={w}
-                    alt="Second slide"
-                />
-
-                <Carousel.Caption>
-                    <div className="comment com-con">
-                        <FormatQuoteRoundedIcon className="quote" />
-                        <div className="mx-5 comment-text">
-                            This is awesome i like very much!
-                            <br />
-                            <StarIcon />
-                            <StarIcon />
-                            <StarIcon />
-                            <StarIcon />
-                            <StarHalfIcon />
+                </div>
+                <div class="carousel-item">
+                    <img
+                        className="d-block w-100"
+                        src="./food3.jpg"
+                        style={w}
+                        alt="Second slide"
+                    />
+                    <div class="carousel-caption d-none d-md-block">
+                        <div className="comment com-con">
+                            <FormatQuoteRoundedIcon className="quote" />
+                            <div className="mx-5 comment-text">
+                                This is awesome i like very much!
+                                <br />
+                                <StarIcon />
+                                <StarIcon />
+                                <StarIcon />
+                                <StarIcon />
+                                <StarHalfIcon />
+                            </div>
+                            <FormatQuoteRoundedIcon className="quote2" />
                         </div>
-                        <FormatQuoteRoundedIcon className="quote2" />
                     </div>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                    className="d-block w-100"
-                    src="./statement.jpg"
-                    style={w}
-                    alt="Third slide"
-                />
-
-                <Carousel.Caption>
-                    <div className="comment com-con">
-                        <FormatQuoteRoundedIcon className="quote" />
-                        <div className="mx-5 comment-text">
-                            This is awesome i like very much!
-                            <br />
-                            <StarIcon />
-                            <StarIcon />
-                            <StarIcon />
-                            <StarIcon />
-                            <StarHalfIcon />
+                </div>
+                <div class="carousel-item">
+                    <img
+                        className="d-block w-100"
+                        src="./statement.jpg"
+                        style={w}
+                        alt="Third slide"
+                    />
+                    <div class="carousel-caption d-none d-md-block">
+                        <div className="comment com-con">
+                            <FormatQuoteRoundedIcon className="quote" />
+                            <div className="mx-5 comment-text">
+                                This is awesome i like very much!
+                                <br />
+                                <StarIcon />
+                                <StarIcon />
+                                <StarIcon />
+                                <StarIcon />
+                                <StarHalfIcon />
+                            </div>
+                            <FormatQuoteRoundedIcon className="quote2" />
                         </div>
-                        <FormatQuoteRoundedIcon className="quote2" />
                     </div>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
+                </div>
+            </div>
+            <button
+                class="carousel-control-prev"
+                type="button"
+                data-mdb-target="#carouselExampleCaptions"
+                data-mdb-slide="prev"
+            >
+                <span
+                    class="carousel-control-prev-icon"
+                    aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button
+                class="carousel-control-next"
+                type="button"
+                data-mdb-target="#carouselExampleCaptions"
+                data-mdb-slide="next"
+            >
+                <span
+                    class="carousel-control-next-icon"
+                    aria-hidden="true"
+                ></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
     );
 }
 
@@ -138,6 +189,7 @@ function Homepage() {
                 <Middle2 />
                 <Middle1 />
                 <Middle3 />
+                <Comment />
             </Container>
         </div>
     );
