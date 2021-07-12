637161a1d1b161242ef759a62a479b363d43bb2a
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-07-12 16:13:57 +0530

    Corrected the merge mistake.

--- src/HotelX/Starters.js
+++ src/HotelX/Starters.js
@@ -8,25 +8,7 @@ import AddShoppingCartIcon from "@material-ui/icons/AddShoppingCart";
 const auth = firebase.auth();
 function Starters() {
     const [user] = useAuthState(auth);
-    const[photos,setPhotos]= useState([]);
-    
-    useEffect(() => {
-        db.collection("dishes").doc("dish").collection("Starters").onSnapshot(
-            snapshot => {
-                setPhotos(snapshot.docs.map(doc => doc.data()))
-            }
-        )
-        }, []);
-        const addToCart= (e,index,item)=>{
-                console.log(index)
-                db.collection("users").doc(user.uid).collection("My-cart").add({
-                    price:item.price,
-                    name:item.name
-
-                })
-
     const [photos, setPhotos] = useState([]);
-
 
     useEffect(() => {
         db.collection("dishes")
@@ -36,31 +18,41 @@ function Starters() {
                 setPhotos(snapshot.docs.map((doc) => doc.data()));
             });
     }, []);
-    const addToCart = (e, index) => {
+    const addToCart = (e, index, item) => {
         console.log(index);
         db.collection("users").doc(user.uid).collection("My-cart").add({
-            photo: photos[index],
+            price: item.price,
+            name: item.name,
         });
     };
+
     console.log(photos);
     return (
         <div className="_starters">
-
-            {photos.map((item,index) => (
-   <Card key={index} className="_box col-sm-12 ">
-      <img src={ item.url } className=" _imgS" style={{filter: 'none',height:"30vh",width:"30vh"}} />
-      <Typography variant="h5" component="h2" className="_typo">
-            How are you ?
-            {item.name}
-            {item.price}
-          </Typography>
-          <AddShoppingCartIcon style={{ color: "#7b877c", size: 0.5 }} className="_cart" onClick={(e)=>addToCart(e,index,item)}/>
-   </Card>
-))}
-            
+            {photos.map((item, index) => (
+                <Card key={index} className="_box col-sm-12 ">
+                    <img
+                        src={item.url}
+                        className=" _imgS"
+                        style={{
+                            filter: "none",
+                            height: "30vh",
+                            width: "30vh",
+                        }}
+                    />
+                    <Typography variant="h5" component="h2" className="_typo">
+                        How are you ?{item.name}
+                        {item.price}
+                    </Typography>
+                    <AddShoppingCartIcon
+                        style={{ color: "#7b877c", size: 0.5 }}
+                        className="_cart"
+                        onClick={(e) => addToCart(e, index, item)}
+                    />
+                </Card>
+            ))}
         </div>
     );
-}
 }
 
 export default Starters;
