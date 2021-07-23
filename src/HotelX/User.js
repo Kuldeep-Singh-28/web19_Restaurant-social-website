e4c0f06962575ee1883100ee06ca256e24080fc4
Kiran Tirunagiri <me190003064@iiti.ac.in>
2021-07-24 03:05:09 +0530

    Temporaray users page.

--- src/HotelX/User.js
+++ src/HotelX/User.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Style from "./styles/Users.module.css";
 
 const User = () => {
 	return (
-		<div>
-			<h1>This is the users page.</h1>
+		<div className={Style.master}>
+			<h1 className="display-2">This is the users page.</h1>
 		</div>
 	);
 };
